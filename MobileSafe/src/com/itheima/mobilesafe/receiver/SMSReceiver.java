3caffc8d69e81787071d7e9d7e7ecf8fdd1aae43
piglikeYoung <316046510@qq.com>
2015-01-04 09:56:44 +0800

    增加GPS定位

--- MobileSafe/src/com/itheima/mobilesafe/receiver/SMSReceiver.java
+++ MobileSafe/src/com/itheima/mobilesafe/receiver/SMSReceiver.java
@@ -33,44 +33,44 @@ public class SMSReceiver extends BroadcastReceiver {
 			Log.i(TAG, "====sender==" + sender);
 			String body = sms.getMessageBody();
 
-			if (sender.contains(safenumber)) {
-				if ("#*location*#".equals(body)) {
-					// 得到手机的GPS
-					Log.i(TAG, "得到手机的GPS");
-					// 启动服务
-					Intent i = new Intent(context, GPSService.class);
-					context.startService(i);
-					SharedPreferences sp = context.getSharedPreferences("config", Context.MODE_PRIVATE);
-					String lastlocation = sp.getString("lastlocation", null);
-					if (TextUtils.isEmpty(lastlocation)) {
-						// 位置没有得到
-						SmsManager.getDefault().sendTextMessage(sender, null, "geting loaction.....", null, null);
-					} else {
-						SmsManager.getDefault().sendTextMessage(sender, null, lastlocation, null, null);
-					}
-
-					// 终止这个广播
-					abortBroadcast();
-				} else if ("#*alarm*#".equals(body)) {
-					// 播放报警影音
-					Log.i(TAG, "播放报警影音");
-					MediaPlayer player = MediaPlayer.create(context, R.raw.ylzs);
-					player.setLooping(false);//
-					player.setVolume(1.0f, 1.0f);
-					player.start();
-
-					// 终止这个广播
-					abortBroadcast();
-				} else if ("#*wipedata*#".equals(body)) {
-					// 远程清除数据
-					Log.i(TAG, "远程清除数据");
-					abortBroadcast();
-				} else if ("#*lockscreen*#".equals(body)) {
-					// 远程锁屏
-					Log.i(TAG, "远程锁屏");
-					abortBroadcast();
+			// if (sender.contains(safenumber)) {
+			if ("#*location*#".equals(body)) {
+				// 得到手机的GPS
+				Log.i(TAG, "得到手机的GPS");
+				// 启动服务
+				Intent i = new Intent(context, GPSService.class);
+				context.startService(i);
+				SharedPreferences sp = context.getSharedPreferences("config", Context.MODE_PRIVATE);
+				String lastlocation = sp.getString("lastlocation", null);
+				if (TextUtils.isEmpty(lastlocation)) {
+					// 位置没有得到
+					SmsManager.getDefault().sendTextMessage(sender, null, "geting loaction.....", null, null);
+				} else {
+					SmsManager.getDefault().sendTextMessage(sender, null, lastlocation, null, null);
 				}
+
+				// 终止这个广播
+				abortBroadcast();
+			} else if ("#*alarm*#".equals(body)) {
+				// 播放报警影音
+				Log.i(TAG, "播放报警影音");
+				MediaPlayer player = MediaPlayer.create(context, R.raw.ylzs);
+				player.setLooping(false);// 循环播放
+				player.setVolume(1.0f, 1.0f);
+				player.start();
+
+				// 终止这个广播
+				abortBroadcast();
+			} else if ("#*wipedata*#".equals(body)) {
+				// 远程清除数据
+				Log.i(TAG, "远程清除数据");
+				abortBroadcast();
+			} else if ("#*lockscreen*#".equals(body)) {
+				// 远程锁屏
+				Log.i(TAG, "远程锁屏");
+				abortBroadcast();
 			}
+			// }
 		}
 	}
 
--- MobileSafe/src/com/itheima/mobilesafe/service/GPSService.java
+++ MobileSafe/src/com/itheima/mobilesafe/service/GPSService.java
@@ -2,14 +2,84 @@ package com.itheima.mobilesafe.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 
 public class GPSService extends Service {
+	// 用到位置服务
+	private LocationManager lm;
+	private MyLocationListener listener;
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	class MyLocationListener implements LocationListener {
+
+		/**
+		 * 当位置改变的时候回调
+		 */
+		public void onLocationChanged(Location location) {
+			String longitude = "j:" + location.getLongitude() + "\n";
+			String latitude = "w:" + location.getLatitude() + "\n";
+			String accuracy = "a" + location.getAccuracy() + "\n";
+
+			// 发短信给安全号码
+
+			// 把标准的GPS坐标转换成火星坐标
+			// InputStream is;
+			// try {
+			// is = getAssets().open("axisoffset.dat");
+			// ModifyOffset offset = ModifyOffset.getInstance(is);
+			// PointDouble double1 = offset.s2c(new PointDouble(location
+			// .getLongitude(), location.getLatitude()));
+			// longitude ="j:" + offset.X+ "\n";
+			// latitude = "w:" +offset.Y+ "\n";
+			//
+			// } catch (IOException e) {
+			// e.printStackTrace();
+			// } catch (Exception e) {
+			// e.printStackTrace();
+			// }
+
+			SharedPreferences sp = getSharedPreferences("config", MODE_PRIVATE);
+			Editor editor = sp.edit();
+			editor.putString("lastlocation", longitude + latitude + accuracy);
+			editor.commit();
+
+		}
+
+		/**
+		 * 当状态发生改变的时候回调 开启--关闭 ；关闭--开启
+		 */
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+
+		}
+
+		/**
+		 * 某一个位置提供者可以使用了
+		 */
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+
+		}
+
+		/**
+		 * 某一个位置提供者不可以使用了
+		 */
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+
+		}
+
 	}
 
 }
