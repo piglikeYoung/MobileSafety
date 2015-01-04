cbe5559afe3974e9cadcd869dc6b2324a4f24ad4
piglikeYoung <316046510@qq.com>
2015-01-04 12:03:47 +0800

    完成GPS定位

--- MobileSafe/src/com/itheima/mobilesafe/service/GPSService.java
+++ MobileSafe/src/com/itheima/mobilesafe/service/GPSService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,8 +18,37 @@ public class GPSService extends Service {
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+		listener = new MyLocationListener();
+
+		// 注册监听位置服务
+		// 给位置提供者设置条件
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_FINE);// 精确程度
+
+		// 设置参数细化：
+		// criteria.setAccuracy(Criteria.ACCURACY_FINE);//设置为最大精度
+		// criteria.setAltitudeRequired(false);//不要求海拔信息
+		// criteria.setBearingRequired(false);//不要求方位信息
+		// criteria.setCostAllowed(true);//是否允许付费
+		// criteria.setPowerRequirement(Criteria.POWER_LOW);//对电量的要求
+
+		String proveder = lm.getBestProvider(criteria, true);// 最好提供者，网络好的时候A-GPS，不好的时候GPS
+		lm.requestLocationUpdates(proveder, 0, 0, listener);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		// 取消监听位置服务
+		lm.removeUpdates(listener);
+		listener = null;
 	}
 
 	class MyLocationListener implements LocationListener {
@@ -27,9 +57,9 @@ public class GPSService extends Service {
 		 * 当位置改变的时候回调
 		 */
 		public void onLocationChanged(Location location) {
-			String longitude = "j:" + location.getLongitude() + "\n";
-			String latitude = "w:" + location.getLatitude() + "\n";
-			String accuracy = "a" + location.getAccuracy() + "\n";
+			String longitude = "j:" + location.getLongitude() + "\n";// 经度
+			String latitude = "w:" + location.getLatitude() + "\n";// 纬度
+			String accuracy = "a:" + location.getAccuracy() + "\n";// 精确度
 
 			// 发短信给安全号码
 
@@ -40,8 +70,7 @@ public class GPSService extends Service {
 			// ModifyOffset offset = ModifyOffset.getInstance(is);
 			// PointDouble double1 = offset.s2c(new PointDouble(location
 			// .getLongitude(), location.getLatitude()));
-			// longitude ="j:" + offset.X+ "\n";
-			// latitude = "w:" +offset.Y+ "\n";
+			// System.out.println(double1);
 			//
 			// } catch (IOException e) {
 			// e.printStackTrace();
@@ -60,7 +89,6 @@ public class GPSService extends Service {
 		 * 当状态发生改变的时候回调 开启--关闭 ；关闭--开启
 		 */
 		public void onStatusChanged(String provider, int status, Bundle extras) {
-			// TODO Auto-generated method stub
 
 		}
 
@@ -68,7 +96,6 @@ public class GPSService extends Service {
 		 * 某一个位置提供者可以使用了
 		 */
 		public void onProviderEnabled(String provider) {
-			// TODO Auto-generated method stub
 
 		}
 
@@ -76,7 +103,6 @@ public class GPSService extends Service {
 		 * 某一个位置提供者不可以使用了
 		 */
 		public void onProviderDisabled(String provider) {
-			// TODO Auto-generated method stub
 
 		}
 
